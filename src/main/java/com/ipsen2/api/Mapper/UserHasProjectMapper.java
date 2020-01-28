357aeab8380c117fea6aedb0c50960a1a1a073d1
mmbasgol <mmbasgol@hotmail.com>
2020-01-28 22:57:15 +0100

    projectid naar int veranderd

--- src/main/java/com/ipsen2/api/Mapper/UserHasProjectMapper.java
+++ src/main/java/com/ipsen2/api/Mapper/UserHasProjectMapper.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class UserHasProjectMapper implements ResultSetMapper<UserHasProject> {
     @Override
     public UserHasProject map(int i, ResultSet resultSet, StatementContext statementContext) throws SQLException {
-        return new UserHasProject(resultSet.getString("user_id"), resultSet.getString("project_id"));
+        return new UserHasProject(resultSet.getString("user_id"), resultSet.getInt("project_id"));
     }
 }
 
